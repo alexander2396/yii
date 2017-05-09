aa3e7c4d82f34bb30ed3cb569ae4e0457f18de70
alex <alex@alex>
2017-05-09 21:07:36 +0300

    Lessons

--- backend/assets/LoginAsset.php
+++ backend/assets/LoginAsset.php
@@ -12,10 +12,12 @@ class LoginAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        //'css/site.css',
+        'css/bootstrap.min.css',
+        'css/AdminLTE.min.css',
+        'plugins/iCheck/flat/blue.css',
     ];
     public $js = [
-
+        'plugins/iCheck/icheck.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Здравствуйте!</h1>
+        <h1>Админка</h1>
 
-        <p class="lead">Здесь вы можете получить ответы на вопросы.</p>
+        <p class="lead">Админка сайта</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
